06639915eedd380a5ce8b0004cca209d486127ff
Soob_home <water4360@naver.com>
2023-06-18 20:00:53 +0900

    NoticeVO 추가

--- src/main/java/board/NoticeVO.java
+++ src/main/java/board/NoticeVO.java
@@ -7,4 +7,7 @@ public class NoticeVO {
 	private String contents;
 	private String regDate;
 	private int hit;
+	private String category; //공지사항/자주묻는질문
+//	private String fileNo;
+//	private String fileName;
 }
